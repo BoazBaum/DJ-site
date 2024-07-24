631168e7e150a0b0b53a977ac155b04de67887b5
Boaz <boazbaum100@gmail.com>
2024-07-24 20:02:29 +0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+//import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -31,7 +31,7 @@ import { VideosComponent } from './videos/videos.component';
     BrowserModule,
     AppRoutingModule,
     YouTubePlayerModule,
-    HttpClientModule,
+   // HttpClientModule,
     FormsModule
   ],
   providers: [],
